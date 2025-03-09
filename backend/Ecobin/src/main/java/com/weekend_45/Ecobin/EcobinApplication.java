695535b9c0791a54a812311a65d29f3ac30926a7
RavinduThilinaka <ravindu2001thilinaka@gmail.com>
2025-03-09 16:32:03 +0530

    add to backend dependancy

---
+++ backend/Ecobin/src/main/java/com/weekend_45/Ecobin/EcobinApplication.java
@@ -0,0 +1,13 @@
+package com.weekend_45.Ecobin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcobinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcobinApplication.class, args);
+	}
+
+}
---
+++ backend/Ecobin/src/test/java/com/weekend_45/Ecobin/EcobinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weekend_45.Ecobin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcobinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
