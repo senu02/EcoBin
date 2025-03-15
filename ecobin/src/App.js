4ffbe3ad4ce8cd46a0a148ce2423652ca593b9c4
Praveen pramodh <149309291+praveenpramodh2002@users.noreply.github.com>
2025-03-16 00:46:19 +0530

    request

--- ecobin/src/App.js
+++ ecobin/src/App.js
@@ -18,6 +18,8 @@ import CollectionScheduleView from './Dashboard/CollectionScheduleView';
 import CollectionSchduleReport from './Dashboard/CollectionSchduleReport';
 import CollectionScheduleGenarateReport from './Dashboard/CollectionScheduleGenarateReport';
 import Navbar from './Navbar/NavBar';
+ 
+import WastePickupRequest from './Home/RequestWaste';
 
 export default function App() {
   return (
@@ -29,9 +31,25 @@ export default function App() {
             <>
               <Navbar /> {/* Navbar will only appear on the Home page */}
               <Home />
+              
+              
             </>
+            
           }
         />
+         <Route
+          path="/WastePickupRequest"
+          element={
+            <>
+              <Navbar /> {/* Navbar will only appear on the Home page */}
+              <WastePickupRequest />
+              
+              
+            </>
+            
+          }
+        />
+         
         <Route path="/SignupForm" element={<SignupForm/>}/>
         <Route path="/Login" element={<Login/>}/>
         <Route path="/WasteReport" element={<WasteReport/>}/>
@@ -47,6 +65,9 @@ export default function App() {
         <Route path="/collectionupdate/:id" element={<CollectionScheduleUpdate/>}/>
         <Route path="/Collectionanalythics" element={<CollectionScheduleAnalytics/>}/>
         <Route path="/CollectionGenarateReport" element={<CollectionScheduleGenarateReport/>}/>
+
+
+        <Route path="/WastePickupRequest"  element={<WastePickupRequest/>}/>
       </Routes>
     </Router>
   );
